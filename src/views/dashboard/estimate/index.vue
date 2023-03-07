5bdd9eb345f1102b1312c1a4688205d9ac75b86c
oreomaker <zh002919@outlook.com>
2023-03-07 22:18:39 +0800

    feat: setup step form for estimation view

--- src/views/dashboard/estimate/index.vue
+++ src/views/dashboard/estimate/index.vue
@@ -1,134 +1,117 @@
 <template>
   <div class="container">
     <Breadcrumb :items="['menu.dashboard', 'menu.dashboard.estimate']" />
-    <a-form
-      ref="formRef"
-      layout="vertical"
-      :model="form"
-      @submit="handleSubmit"
-    >
-      <a-space direction="vertical" :size="16">
-        <a-card class="general-card">
-          <template #title>
-            {{ $t('dashboard.title.document') }}
-          </template>
-          <a-row :gutter="80">
-            <a-col :span="8">
-              <a-form-item
-                :label="$t('dashboard.form.label.location')"
-                field="location"
-              >
-                <a-select
-                  v-model="form.location"
-                  :placeholder="$t('groupForm.placeholder.video.mode')"
-                >
-                  <a-option value="Beijing">北京</a-option>
-                  <a-option value="Shanghai">上海</a-option>
-                  <a-option value="Shenzhen">深圳</a-option>
-                </a-select>
-              </a-form-item>
-            </a-col>
-            <a-col :span="8">
-              <a-form-item
-                :label="$t('groupForm.form.label.video.acquisition.resolution')"
-                field="factor1"
-              >
-                <a-select
-                  v-model="form.factor1"
-                  :placeholder="$t('dashboard.form.label.factor1')"
-                >
-                  <a-option value="resolution1">分辨率1</a-option>
-                  <a-option value="resolution2">分辨率2</a-option>
-                  <a-option value="resolution3">分辨率3</a-option>
-                </a-select>
-              </a-form-item>
-            </a-col>
-            <a-col :span="8">
-              <a-form-item
-                :label="$t('dashboard.form.label.factor2')"
-                field="factor2"
-              >
-                <a-input
-                  v-model="form.factor2"
-                  :placeholder="
-                    $t('groupForm.placeholder.video.acquisition.frameRate')
-                  "
-                >
-                  <template #append> ms </template>
-                </a-input>
-              </a-form-item>
-            </a-col>
-          </a-row>
-        </a-card>
-        <a-card class="general-card" :bordered="true" style="padding-top: 20px">
-          <a-upload draggable :action="uploadUrl" />
-        </a-card>
-      </a-space>
-      <!-- TODO: for debug -->
-      {{ form }}
-      {{ costEstimate }}
-      <div class="actions">
-        <a-space>
-          <a-button>
-            {{ $t('groupForm.reset') }}
-          </a-button>
-          <a-button type="primary" :loading="loading" html-type="submit">
-            {{ $t('groupForm.submit') }}
-          </a-button>
-        </a-space>
-      </div>
-    </a-form>
+    <a-spin :loading="loading" style="width: 100%">
+      <a-card class="general-card">
+        <div class="wrapper">
+          <a-steps
+            v-model:current="step"
+            style="width: 580px"
+            line-less
+            class="steps"
+          >
+            <a-step :description="$t('stepForm.step.document.info')">
+              {{ $t('stepForm.step.document') }}
+            </a-step>
+            <a-step :description="$t('stepForm.step.review.info')">
+              {{ $t('stepForm.step.review') }}
+            </a-step>
+            <a-step :description="$t('stepForm.step.estimate.info')">
+              {{ $t('stepForm.step.estimate') }}
+            </a-step>
+          </a-steps>
+          <keep-alive>
+            <BaseInfo v-if="step === 1" @change-step="changeStep" />
+            <ChannelInfo v-else-if="step === 2" @change-step="changeStep" />
+            <Success v-else-if="step === 3" @change-step="changeStep" />
+          </keep-alive>
+        </div>
+      </a-card>
+    </a-spin>
   </div>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue';
   import useLoading from '@/hooks/loading';
-  import { estimate, EstimateData } from '@/api/estimate';
+  import {
+    submitChannelForm,
+    BaseInfoModel,
+    ChannelInfoModel,
+    UnitChannelModel,
+  } from '@/api/form';
+  import BaseInfo from './components/base-info.vue';
+  import ChannelInfo from './components/channel-info.vue';
+  import Success from './components/success.vue';
 
-  const { loading, setLoading } = useLoading();
-  const uploadUrl = ref('/estimate/upload');
-  const form = ref<EstimateData>({
-    location: '',
-    factor1: '',
-    factor2: '',
-  });
-  const costEstimate = ref(0);
-  const handleSubmit = async () => {
+  const { loading, setLoading } = useLoading(false);
+  const step = ref(1);
+  const submitModel = ref<UnitChannelModel>({} as UnitChannelModel);
+  const submitForm = async () => {
     setLoading(true);
-    const res = await estimate(form.value);
-    // eslint-disable-next-line no-console
-    console.log(res);
-    costEstimate.value = res?.data.cost;
-    if (!res) {
-      setLoading(true);
-    }
-    setTimeout(() => {
+    try {
+      await submitChannelForm(submitModel.value); // The mock api default success
+      step.value = 3;
+      submitModel.value = {} as UnitChannelModel; // init
+    } catch (err) {
+      // you can report use errorHandler or other
+    } finally {
       setLoading(false);
-    }, 1000);
+    }
+  };
+  const changeStep = (
+    direction: string | number,
+    model: BaseInfoModel | ChannelInfoModel
+  ) => {
+    if (typeof direction === 'number') {
+      step.value = direction;
+      return;
+    }
+
+    if (direction === 'forward' || direction === 'submit') {
+      submitModel.value = {
+        ...submitModel.value,
+        ...model,
+      };
+      if (direction === 'submit') {
+        submitForm();
+        return;
+      }
+      step.value += 1;
+    } else if (direction === 'backward') {
+      step.value -= 1;
+    }
   };
 </script>
 
 <script lang="ts">
   export default {
-    name: 'Estimate',
+    name: 'Step',
   };
 </script>
 
 <style scoped lang="less">
   .container {
-    padding: 0 20px 40px;
-    overflow: hidden;
+    padding: 0 20px 20px;
   }
 
-  .actions {
-    position: fixed;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    height: 60px;
-    padding: 14px 20px 14px 0;
-    text-align: right;
-    background: var(--color-bg-2);
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 64px 0;
+    background-color: var(--color-bg-2);
+
+    :deep(.arco-form) {
+      .arco-form-item {
+        &:last-child {
+          margin-top: 20px;
+        }
+      }
+    }
+  }
+
+  .steps {
+    margin-bottom: 76px;
   }
 </style>
